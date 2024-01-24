e5aab9a3820c4dda06ec325ffd8212074b6569c7
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-01-24 13:04:16 +0100

    Initial Commit

---
+++ CloudTest/CloudTest.h
@@ -0,0 +1,18 @@
+//
+//  CloudTest.h
+//  CloudTest
+//
+//  Created by Felix Pultar on 24.01.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudTest.
+FOUNDATION_EXPORT double CloudTestVersionNumber;
+
+//! Project version string for CloudTest.
+FOUNDATION_EXPORT const unsigned char CloudTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudTest/PublicHeader.h>
+
+
